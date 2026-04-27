be8b4b01aed96f375f5a8efa85f3d33f60bdd4ca
송수빈 <soobin@songsubin-ui-MacBookAir.local>
2026-04-27 13:32:28 +0900

    feat:part12 ch02-3.정수형,실수형,boolean타입 실습

---
+++ variable1.py
@@ -0,0 +1,32 @@
+# 주석을 입력할 수 있습니다
+# integer_variable=42
+
+#print(integer_variable) #42
+
+var = 42
+#print(var) #42
+
+#print(var+10)
+
+#print(var-5)
+
+#print(var/10)
+
+#print(var // 10)
+
+#print(var % 10)
+
+var2= var/10
+
+#print(var>var2)
+
+var_float=3.14
+#priint(3.14 * 6)
+#print(3.14/2)
+
+is_true = True
+is_false=False
+
+print(is_true and is_true) #true
+print(is_true and is_false) #false
+print(is_false and is_false) #false
